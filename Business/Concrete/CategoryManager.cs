776913a8af925977e52154b6542c1551cc7de357
selman <88801652+selmanagirman7@users.noreply.github.com>
2023-01-03 15:41:10 +0300

    Add files via upload

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity;
+using System;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public void Add(Category category)
+        {
+            _categoryDal.Add(category);
+        }
+
+        public void Delete(Category category)
+        {
+            _categoryDal.Delete(category);
+        }
+
+        public Category Get(int id)
+        {
+            return _categoryDal.GetById(id);
+        }
+
+        public List<Category> GetAll()
+        {
+
+         
+                return _categoryDal.GetAll();
+            
+       
+           
+        }
+
+        public void Update(Category category)
+        {
+
+
+             _categoryDal.Update(category);
+
+            
+         
+        }
+    }
+}
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,61 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public Product Get(int id)
+        {
+            return _productDal.GetById(id);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetTreeProduct()
+        {
+            return _productDal.GetAll().Take(3).ToList();
+        }
+
+
+        public List<Product> GetDetail(int id)
+        {
+            return _productDal.GetList(x => x.ProductId == id).ToList();
+        }
+
+        public List<Product> GetProductById(int id)
+        {
+            return _productDal.List(x => x.ProductId == id);
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}
